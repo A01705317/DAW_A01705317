78f0c9c953488ce51a58088edf49bb91a1fee97c
A01705317 <47190742+A01705317@users.noreply.github.com>
2020-10-28 17:13:00 -0600

    Examen Parcial 2 DAW

--- Examen Parcial 2/2PDAW/sql/bdzombies.sql
+++ Examen Parcial 2/2PDAW/sql/bdzombies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-10-2020 a las 17:50:37
+-- Tiempo de generación: 28-10-2020 a las 21:45:50
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `zombies`
+-- Base de datos: `bdzombies`
 --
 
 -- --------------------------------------------------------
@@ -46,14 +46,38 @@ INSERT INTO `estado` (`id_estado`, `nombre_estado`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `estado_zombie`
+--
+
+CREATE TABLE `estado_zombie` (
+  `fecha_hora` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_zombie` int(11) NOT NULL,
+  `id_estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `zombie`
 --
 
 CREATE TABLE `zombie` (
-  `estado_id` int(11) NOT NULL,
-  `nombre` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id_zombie` int(11) NOT NULL,
+  `nombre_zombie` varchar(40) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `zombie`
+--
+
+INSERT INTO `zombie` (`id_zombie`, `nombre_zombie`) VALUES
+(1, 'Miroslav Klose'),
+(2, 'Thomas Muller'),
+(3, 'Marco Reus'),
+(4, 'Matt Humels'),
+(5, 'Manuel Neuer'),
+(6, 'Jerom Boateng'),
+(7, 'Petter Mertesacken');
 
 --
 -- Índices para tablas volcadas
@@ -66,20 +90,29 @@ ALTER TABLE `estado`
   ADD PRIMARY KEY (`id_estado`);
 
 --
+-- Indices de la tabla `estado_zombie`
+--
+ALTER TABLE `estado_zombie`
+  ADD PRIMARY KEY (`fecha_hora`),
+  ADD KEY `index_zombie` (`id_zombie`),
+  ADD KEY `index_estado` (`id_estado`);
+
+--
 -- Indices de la tabla `zombie`
 --
 ALTER TABLE `zombie`
-  ADD KEY `estado_id` (`estado_id`);
+  ADD PRIMARY KEY (`id_zombie`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `zombie`
+-- Filtros para la tabla `estado_zombie`
 --
-ALTER TABLE `zombie`
-  ADD CONSTRAINT `zombie_ibfk_1` FOREIGN KEY (`estado_id`) REFERENCES `estado` (`id_estado`);
+ALTER TABLE `estado_zombie`
+  ADD CONSTRAINT `estado_zombie_ibfk_1` FOREIGN KEY (`id_zombie`) REFERENCES `zombie` (`id_zombie`),
+  ADD CONSTRAINT `estado_zombie_ibfk_2` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
